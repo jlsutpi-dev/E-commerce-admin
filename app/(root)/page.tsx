0ef8b99b9f8649008efb9e09bb11cbfad2aada09
SutPi0154 <sutpi070@gmail.com>
2024-10-21 10:44:23 +0630

    clerk auth done

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,14 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+const SetupPage = () => {
   return (
     <div className=" p-4">
+      <UserButton />
+
       <Button size={"default"}>click me</Button>
     </div>
   );
-}
+};
+
+export default SetupPage;
